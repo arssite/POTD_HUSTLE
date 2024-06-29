d41a3c5457a59456eef33f10716ca2e801fa0468
Anmol R. Srivastava <91518669+arssite@users.noreply.github.com>
2024-06-29 10:25:30 +0530

    Identical_LinkedList.py

---
+++ Identical_LinkedList.py
@@ -0,0 +1,22 @@
+'''
+# Node Class    
+class node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+        
+'''
+
+#Function to check whether two linked lists are identical or not.
+def areIdentical(head1, head2):
+    # Code here
+    while head1!=None and head2!=None:
+        flag=1
+        if head1.data!=head2.data:
+            flag=0
+            return flag
+        head1,head2=head1.next,head2.next
+        
+    return head1==None and head2==None
+
+
