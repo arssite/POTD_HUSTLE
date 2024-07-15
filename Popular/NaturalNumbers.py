4ef2d2f0472c6e4611101fe61827c4dbe6644c42
Anmol R. Srivastava <91518669+arssite@users.noreply.github.com>
2024-07-15 15:44:05 +0530

    NaturalNumbers.py

---
+++ Popular/NaturalNumbers.py
@@ -0,0 +1,14 @@
+#sum of natural numbers
+n=int(input())
+x=(n*(n+1))//2
+print(x)
+
+#sum of sq of natural numbers:
+n=int(input())
+x=(n*(n+1)*(2*n+1))//6
+print(x)
+
+#sum of cube of natural number
+n=int(input())
+x=((n*(n+1))//2)**2
+print(x)
