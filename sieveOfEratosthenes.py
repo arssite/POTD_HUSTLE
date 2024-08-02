42aa0d4d134375fc0cf849cbd9d807dd0302750b
Anmol R. Srivastava <91518669+arssite@users.noreply.github.com>
2024-08-02 04:54:59 -0700

    Update sieveOfEratosthenes.py

--- sieveOfEratosthenes.py
+++ sieveOfEratosthenes.py
@@ -17,16 +17,23 @@ class Solution:
 # https://www.geeksforgeeks.org/problems/sieve-of-eratosthenes5242/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
 
 class Solution:
-    def countPrimes(self, n: int) -> int:
-        prime = [1] * n
-        for i in range(2, int(n**0.5) + 1):
-            if prime[i] == 1:
-                for j in range(i*i, n, i):
-                    prime[j] = 0
-        
-        ans = 0
-        for i in range(2, n):
-            if prime[i] == 1:
-                ans += 1
-        
-        return ans
+    def prime_Sum(self, n):
+        if n < 2:
+            return 0
+
+        is_prime = [True] * (n + 1)
+        is_prime[0], is_prime[1] = False, False
+
+        p = 2
+        while p * p <= n:
+            if is_prime[p]:
+                for i in range(p * p, n + 1, p):
+                    is_prime[i] = False
+            p += 1
+
+        prime_sum = sum(i for i in range(n + 1) if is_prime[i])
+        return prime_sum
+
+# Example usage:
+sol = Solution()
+print(sol.prime_Sum(50))  # Example to get the sum of all primes up to 50
