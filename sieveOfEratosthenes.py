984d3ad0020d8d1181563ce70897364bc67b7c9c
Anmol R. Srivastava <91518669+arssite@users.noreply.github.com>
2024-08-02 03:07:01 -0700

    Update sieveOfEratosthenes.py

--- sieveOfEratosthenes.py
+++ sieveOfEratosthenes.py
@@ -13,3 +13,20 @@ class Solution:
     	    if prim(i):
     	        out.append(i)
     	return out
+
+# https://www.geeksforgeeks.org/problems/sieve-of-eratosthenes5242/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
+
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        prime = [1] * n
+        for i in range(2, int(n**0.5) + 1):
+            if prime[i] == 1:
+                for j in range(i*i, n, i):
+                    prime[j] = 0
+        
+        ans = 0
+        for i in range(2, n):
+            if prime[i] == 1:
+                ans += 1
+        
+        return ans
