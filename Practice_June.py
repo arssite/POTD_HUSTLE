c4e1ef6e72e4ae61236ecb9811fa0747f016b5be
Anmol R. Srivastava <91518669+arssite@users.noreply.github.com>
2026-06-06 16:23:36 +0530

    Add logic to check for dominant elements in array

--- Practice_June.py
+++ Practice_June.py
@@ -10,3 +10,14 @@ for _ in range(int(input())):
             dic[i]=1
     print(n-max(dic.values()))
 
+Q:https://www.codechef.com/practice/course/arrays-strings-sorting/INTARR01/problems/DOMINANT2
+for _ in range(int(input())):
+    n=int(input())
+    lst=map(int,input().split())
+    items=[0]*(n+1)
+    # print(items)
+    for i in lst:
+        items[i]+=1
+        # print(items)
+    max_i=max(items)
+    print('yes' if items.count(max_i)==1 else 'no')
